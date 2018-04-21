e854b5d0a9958b3d8aa854a68a6fe39fd53ca9b5
Shinkay <shinkayb@gmail.com>
2018-04-21 12:45:40 -0400

    amends

--- keys.js
+++ keys.js
@@ -5,11 +5,6 @@ exports.twitter = {
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET, 
-
-// consumer_key: 'IGglzDQ7sitkQk4uKixxzcyJR',
-// consumer_secret:	'WoH3wGD1DvXAVvfATenrRhHGGq7G7D8gfefiPHgyXMiBXkCWz3',
-// access_token:	'987483826325803009-gvkl2m7hDEEQi1148Px7b4O2SE0SkYW',
-// access_token_secret: 'eSDQYXbKBdUznGxJznqsOE92DgydqAnV7PPQQ90uYBImu',
 };
 
 exports.spotify = {
@@ -17,6 +12,6 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-exports.request = {
-    key: '32ec1d67',
-}
+// exports.request = {
+//     key: '32ec1d67',
+// }
--- liri.js
+++ liri.js
@@ -1,7 +1,4 @@
 require("dotenv").config();
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
-
 
 var keys = require('./keys.js');
 
@@ -40,19 +37,19 @@ var getArtistNames = function (artist) {
 }
 
 
- var getMeSpotify = function (songName) {
-     if (songName === undefined) {
-         songName = "whats my age";
-     }
-
- 
-spotify.search({ type: 'track', query: songName }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
+var getMeSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "whats my age";
     }
+
+
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
         var songs = data.tracks.items;
-        for(var i=0; i<songs.length; i++) {
+        for (var i = 0; i < songs.length; i++) {
             console.log(i);
             console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
             console.log('song name: ' + songs[i].name);
@@ -60,66 +57,70 @@ spotify.search({ type: 'track', query: songName }, function(err, data) {
             console.log('album: ' + songs[i].album.name);
 
         }
-});
+    });
 }
 
 var getMovie = function (movieName) {
 
 
-request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-if (!error && response.statusCode == 200){
-    var jsonData = JSON.parse(body);
-    console.log('Title: ', + jsonData.Title);
-    console.log('Year: ', + jsonData.Year);
-    console.log('IMDB Rating: ', + jsonData.imdbRating);
-    console.log('Rotten Tomatoes Rating: ', + jsonData.tomatoRating);
-    console.log('Country: ', + jsonData.Country);
-    console.log('Language: ', + jsonData.Language);
-    console.log('Plot: ', + jsonData.Plot);
-    console.log('Actors: ', + jsonData.Actors);
-  
+    request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        if (!error && response.statusCode == 200) {
+            var jsonData = JSON.parse(body);
+            console.log('Title: ', + jsonData.Title);
+            console.log('Year: ', + jsonData.Year);
+            console.log('IMDB Rating: ', + jsonData.imdbRating);
+            console.log('Rotten Tomatoes Rating: ', + jsonData.tomatoRating);
+            console.log('Country: ', + jsonData.Country);
+            console.log('Language: ', + jsonData.Language);
+            console.log('Plot: ', + jsonData.Plot);
+            console.log('Actors: ', + jsonData.Actors);
+
+        }
+
+    });
 }
-  
-});
-}
 
 
 
-var doWhatItSays = function() {
+var doWhatItSays = function () {
     fs.readFile('random.txt', 'utf8', function (err, data) {
         if (err) throw err;
 
         var dataArr = data.split(',');
 
-        if (dataArr.length ==2){
-            pick(dataArr[0],dataArr[1]);
-        }else if (dataArr.length ==1){
+        if (dataArr.length == 2) {
+            pick(dataArr[0], dataArr[1]);
+        } else if (dataArr.length == 1) {
             pick(dataArr[0]);
         }
-        });
+    });
 }
 
 
-var pick = function(caseData, functionData) {
-    switch(caseData){
-        case 'my-tweets' :
-        getMyTweets();
-        break;
-        case 'spotify-this-song' :
-        getMeSpotify(functionData);
-        break;
+var pick = function (caseData, functionData) {
+    
+    console.log('caseData:', caseData)
+
+    switch (caseData) {
+        case 'my-tweets':
+            getMyTweets();
+            break;
+        case 'spotify-this-song':
+            getMeSpotify(functionData);
+            break;
         case 'movie-this':
-        getMovie(functionData);
-        case 'do-what-it-says' :
-        doWhatItSays();
-        break;
+            getMovie(functionData);
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
         default:
-        console.log('LIRI does not know that');
+            console.log('LIRI does not know that');
     }
 }
 
-var runThis = function (argOne, argTwo){
+var runThis = function (argOne, argTwo) {
     pick(argOne, argTwo);
 };
 
